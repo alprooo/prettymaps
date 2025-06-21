ffd198b3d35616a016ec3bf3c00e29a48e2972aa
alproo <programmer.2nd@gmail.com>
2025-06-21 11:12:23 +0800

    chanfge the title

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 presets = prettymaps.presets().to_dict()
 
 # Set the title of the app
-st.title("Map Mantap")
+st.title("Besutiful Maps Generator")
 
 cols = st.columns([1, 2])
 with cols[0]:
